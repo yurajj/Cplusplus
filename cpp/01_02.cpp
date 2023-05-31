4f0fc3c440b06abf8ce5cbbe22a08db3bbc6dc09
yura j <yul_j@naver.com>
2023-05-31 23:52:37 +0900

    Add files via upload

---
+++ cpp/01_02.cpp
@@ -0,0 +1,9 @@
+#include <iostream>            // 헤더파일을 포함시키는 문장 
+void main() 
+{ 
+            /* cout은 출력을 담당하는 객체로서 
+            스트림 삽입 연산자(stream insertion operator)인 
+            <<를 이용해서 ""내부의 문자열을 출력한다. */ 
+
+            std::cout<<"C++ 세계에 오신 것을 환영합니다.\n";
+} 
---
+++ cpp/01_03.cpp
@@ -0,0 +1,7 @@
+#include <iostream>            // 헤더파일을 포함시키는 문장 
+void main() 
+{ 
+  std::cout<<" 이  름 : 성윤정 "<<std::endl; 
+  std::cout<<" 소  속 : 한빛미디어 "<<std::endl; 
+  std::cout<<" 이메일 : techsung@chol.com "<<std::endl; 
+} 
